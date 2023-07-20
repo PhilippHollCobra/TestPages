4a949232eaca4a13b97f7e95cb00f93b5777ebba
philippholl <Philipp.holl@cobra.de>
2023-07-20 08:07:29 +0200

    docker compose

--- src/WebApp.ApiTests/WeatherForecastControllerTests.cs
+++ src/WebApp.ApiTests/WeatherForecastControllerTests.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace WebApp.ApiTests
 {
   public class WeatherForecastControllerTests
   {
-    private readonly HttpClient _httpClient = new () { BaseAddress = new Uri("https://localhost:7267") };
+    private readonly HttpClient _httpClient = new () { BaseAddress = new Uri("http://localhost:9001") };
 
     [Fact]
     public async void GetTestAsync()
@@ -17,11 +19,26 @@ namespace WebApp.ApiTests
       //Arrange
 
       //Act
-      HttpResponseMessage responseMessage = await _httpClient.GetAsync("weatherforecast/GetWeatherForecast");
+      HttpResponseMessage responseMessage = await _httpClient.GetAsync("weatherforecast");
+      string httpContent = await responseMessage.Content.ReadAsStringAsync();
+      IEnumerable<WeatherForecast>? items = JsonSerializer.Deserialize<IEnumerable<WeatherForecast>>(httpContent, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 
       //Assert
       Assert.True(responseMessage.StatusCode == HttpStatusCode.OK);
+      Assert.NotNull(items);
+      Assert.True(items.Any());
 
     }
+  }
+
+  public class WeatherForecast
+  {
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
   }
 }
